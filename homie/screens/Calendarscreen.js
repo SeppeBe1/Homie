66b4b571e4eca40b5051c4bb8da22fd8b1344f10
unknown <seppe.bellens@hotmail.nl>
2023-04-12 16:15:16 +0200

    Build: bottom menu bar

---
+++ homie/screens/Calendarscreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function CalendarScreen({navigation}) {
+  return (
+    <View>
+      <Text>CalendarScreen</Text>
+    </View>
+  )
+}
---
+++ homie/screens/Homescreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Homescreen({navigation}) {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  )
+}
