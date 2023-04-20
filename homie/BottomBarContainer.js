e695dde9068c6516d855a7e4149cf027c2deb371
unknown <seppe.bellens@hotmail.nl>
2023-04-20 11:44:22 +0200

    Feat[menu]: added icon images to menu

--- homie/BottomBarContainer.js
+++ homie/BottomBarContainer.js
@@ -2,6 +2,7 @@ import { View, Text } from 'react-native'
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Image } from 'react-native';
 
 
 //Screens
@@ -25,12 +26,74 @@ const Tab = createBottomTabNavigator();
 export default function BottomBarContainer() {
   return (
     <NavigationContainer>
-        <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Navigator  tabBarOptions={{ showLabel: false }}
+        
+        screenOptions={({ route  }) => ({
+          tabBarIcon: ({ focused }) => {
+            let icon;
+  
+            if (route.name === 'Home') {
+              icon = focused
+                ? require('./assets/icons/home.png')
+                : require('./images/home.png');
+            } else if (route.name === 'Profile') {
+              icon = focused
+                ? require('./images/profile-focused.png')
+                : require('./images/profile.png');
+            }
+  
+            return <Image source={icon} />;
+          },
+        })}
+        >
 
-        <Tab.Screen name={Homename} component={Homescreen}/>
-        <Tab.Screen name={Costsplittername} component={Costsplitterscreen}/>
-        <Tab.Screen name={Behomiename} component={Behomiescreen}/>
-        <Tab.Screen name={Calendarname} component={CalendarScreen}/>
+        <Tab.Screen name={Homename} component={Homescreen}
+            options={{
+              tabBarIcon: ({ focused, color, size }) => (
+                <Image
+                  source={require('./assets/icons/home.png')}
+                  style={{ width: size, height: size }}
+                  resizeMode="contain"
+                />
+              ),
+            }}
+        />
+
+        <Tab.Screen name={Costsplittername} component={Costsplitterscreen} 
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Image
+              source={require('./assets/icons/community.png')}
+              style={{ width: size, height: size }}
+              resizeMode="contain"
+            />
+          ),
+        }}
+        />
+
+        <Tab.Screen name={Behomiename} component={Behomiescreen}
+         options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Image
+              source={require('./assets/icons/costsplit.png')}
+              style={{ width: size, height: size }}
+              resizeMode="contain"
+            />
+          ),
+        }}
+        />
+
+        <Tab.Screen name={Calendarname} component={CalendarScreen}
+         options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Image
+              source={require('./assets/icons/calendar.png')}
+              style={{ width: size, height: size }}
+              resizeMode="contain"
+            />
+          ),
+        }}
+        />
 
         </Tab.Navigator>
     </NavigationContainer>
