a54272be22a7b5eaf3ab99d4598c70af63ed0660
boysangurkha <r0797383@student.thomasmore.be>
2023-04-27 01:24:30 +0200

    Feat: Homepage

--- homie/screens/Homescreen.js
+++ homie/screens/Homescreen.js
@@ -1,60 +1,60 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Button, Image } from 'react-native';
 import { Header, Avatar } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 
-
+import myImage from '../assets/intersect.svg';
 
 export default function Homescreen({navigation}) {
 
   return (
-    <View 
-      style={{
-        backgroundColor: '#160635',
-        height: 215,
-        width: '100%',
-      }}
-    >
-      <Header
-        containerStyle={styles.headerContainer}
-        leftComponent={<Avatar
-          size="medium"
-          rounded
-          source={{
-            uri:
-              'https://i.redd.it/lmwqtxhw9st41.jpg',
-          }}
-          containerStyle={{ width: 46, height: 46 }}
-        />}
-        centerComponent={
-          <View 
-          style={{
-            display: 'flex', 
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '100%'
-          }}
-          >
-            <Button title="Casa" onPress={() => navigation.navigate('Homeaccount')}  />
-          </View>
-        }
-        rightComponent={
-          <View 
+    <View>
+      <View 
+        style={{
+          backgroundColor: '#160635',
+          height: 215,
+          width: '100%',
+        }}
+      >
+        <Header
+          containerStyle={styles.headerContainer}
+          leftComponent={<Avatar
+            size="medium"
+            rounded
+            source={{
+              uri:
+                'https://i.redd.it/lmwqtxhw9st41.jpg',
+            }}
+            containerStyle={{ width: 46, height: 46 }}
+          />}
+          centerComponent={
+            <View 
             style={{
               display: 'flex', 
               justifyContent: 'center',
               alignItems: 'center',
               height: '100%'
             }}
-          >
-            <Icon name="cog" color="white" size={24} onPress={() => alert('Settings')} />
-          </View>
-        }
-      />
-      <View>
-        <Text>Home</Text>
+            >
+              <Button title="Casa" onPress={() => navigation.navigate('Homeaccount')}  />
+            </View>
+          }
+          rightComponent={
+            <View 
+              style={{
+                display: 'flex', 
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100%'
+              }}
+            >
+              <Icon name="cog" color="white" size={24} onPress={() => alert('Settings')} />
+            </View>
+          }
+        />
       </View>
+      <Image source={myImage} style={{width: 100, height: 100}} />
     </View>
   );
 }
