0e1a76fcff50e4e849718a2c39961cf2c3cd3c8e
boysangurkha <r0797383@student.thomasmore.be>
2023-04-27 10:42:31 +0200

    test

--- homie/screens/Homescreen.js
+++ homie/screens/Homescreen.js
@@ -3,20 +3,12 @@ import { StyleSheet, View, Text, Button, Image } from 'react-native';
 import { Header, Avatar } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-
-import myImage from '../assets/intersect.svg';
+import myImage from '../assets/float.svg';
 
 export default function Homescreen({navigation}) {
-
   return (
-    <View>
-      <View 
-        style={{
-          backgroundColor: '#160635',
-          height: 215,
-          width: '100%',
-        }}
-      >
+    <View style={styles.container}>
+      <View style={styles.header}>
         <Header
           containerStyle={styles.headerContainer}
           leftComponent={<Avatar
@@ -30,12 +22,12 @@ export default function Homescreen({navigation}) {
           />}
           centerComponent={
             <View 
-            style={{
-              display: 'flex', 
-              justifyContent: 'center',
-              alignItems: 'center',
-              height: '100%'
-            }}
+              style={{
+                display: 'flex', 
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100%'
+              }}
             >
               <Button title="Casa" onPress={() => navigation.navigate('Homeaccount')}  />
             </View>
@@ -54,12 +46,34 @@ export default function Homescreen({navigation}) {
           }
         />
       </View>
-      <Image source={myImage} style={{width: 100, height: 100}} />
+      <View style={styles.imageContainer}>
+        <Image source={myImage} style={styles.image} />
+        <View style={{position:'absolute', left:120, top:35, zIndex: 10, width:120, height:100, display:'flex', alignItems:'center', justifyContent:'center'}}>
+          <Text style={{fontSize:'1rem'}}>Welcome back Jade!</Text>
+        </View>
+      </View>
+      <View style={{marginTop: 80, paddingHorizontal: 24}}>
+        <View style={{}}>
+          <Text>Announcements</Text>
+          <Text>Add announcement</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    position: 'relative',
+    height: '100%',
+    width: '100%',
+  },
+  header: {
+    backgroundColor: '#160635',
+    height: 215,
+    width: '100%',
+  },
   headerContainer: {
     backgroundColor: '#160635',
     paddingHorizontal: 24,
@@ -67,6 +81,16 @@ const styles = StyleSheet.create({
     border: 'none',
     marginTop: 40,
   },
+  imageContainer: {
+    position: 'absolute',
+    top: 100,
+    left: "-1px",
+    height: '200px',
+    width: '350px',
+  },
+  image: {
+    resizeMode: 'contain',
+    maxWidth: '80%',
+    height: '100%',
+  }
 });
-
-
