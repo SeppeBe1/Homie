1b1c9b00fb0bbfbcf68f93f18cfcd4b8fa58c91d
unknown <seppe.bellens@hotmail.nl>
2023-04-27 07:54:17 +0200

    Feat[homescreen]: add Picker for languages

--- homie/screens/Homescreen.js
+++ homie/screens/Homescreen.js
@@ -1,21 +1,40 @@
-import { StyleSheet, Text, View, Switch } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Switch, Picker } from 'react-native';
+import React, {useState} from 'react';
 
 export default function Homescreen() {
+
+  const [notifications, setNotifications] = useState(false);
+  const notificationsVal = () => setIsEnabled(previousState => !previousState);
+
+  const [nightmode, setnightmode] = useState(false);
+  const nightmodeVal = () => setnightmode(previousState => !previousState);
+
+  const [autooPlay, setAutoPlay] = useState(false);
+  const autoPlay = () => setAutoPlay(previousState => !previousState);
+
+  const [selectedValue, setSelectedValue] = useState("English");
+
   return (
     <View>
     <Text >Profile Settings</Text>
     <View style={styles.switchContainter}>
       <Text>Notifications</Text>
-      <Switch />
+      <Switch onValueChange={notificationsVal} value={notifications} />
     </View>
     <View style={styles.switchContainter}>
-      <Text>Dark Mode</Text>
-      <Switch/>
+      <Text>Nightmode</Text>
+      <Switch onValueChange={nightmodeVal} value={nightmode}/>
     </View>
     <View style={styles.switchContainter}>
-      <Text>Auto-Play</Text>
-       <Switch />
+      <Text>Language</Text>
+      <Picker
+        selectedValue={selectedValue}
+        style={{ height: 20, width: 60, alignItems: "center" ,fontSize: 12, paddingLeft: 'auto', paddingRight: 'auto',}}
+        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+      >
+        <Picker.Item label="ENG" value="English" />
+        <Picker.Item label="NL" value="Nederlands" />
+      </Picker>
     </View>
   </View>
   )
