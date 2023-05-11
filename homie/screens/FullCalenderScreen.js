3d367683232274ca768c0df51c7ad29e86b5f1ae
Jade Apers <jadeapers@telenet.be>
2023-05-11 16:06:18 +0200

    feat: added back button and header

--- homie/screens/FullCalenderScreen.js
+++ homie/screens/FullCalenderScreen.js
@@ -1,12 +1,45 @@
-import { Text, View } from "react-native";
-import React, { Component } from "react";
+import { Text, View, StyleSheet, TouchableOpacity, Image } from "react-native";
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import arrowLeft from "../assets/icons/arrowLeft.svg";
 
-export default class FullCalenderScreen extends Component {
-  render() {
-    return (
-      <View>
-        <Text>FullCalenderScreen</Text>
+export default function FullCalenderScreen() {
+  const navigation = useNavigation();
+
+  return (
+    <View>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Image
+            source={arrowLeft}
+            style={{ width: 8, height: 15, marginRight: 10 }}
+          />
+        </TouchableOpacity>
+        <View style={{ flex: 1, alignItems: "center" }}>
+          <Text
+            style={{
+              color: "#fff",
+              fontFamily: "novaticaBold",
+              fontSize: 20,
+            }}
+          >
+            Settings
+          </Text>
+        </View>
       </View>
-    );
-  }
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    backgroundColor: "#160635",
+    height: "115px",
+    marginBottom: 20,
+  },
+});
