dbbf48a0b6a6b4df55429a173ce4322941b23e48
Yanellevdb <yanelle1@hotmail.com>
2023-06-07 22:57:29 +0200

    fix: fix borderradius issue

--- homie/screens/Memorywall.js
+++ homie/screens/Memorywall.js
@@ -1,276 +1,3 @@
-/*import React, { useState, useEffect } from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  ScrollView,
-  Image,
-  ImageBackground,
-  TouchableOpacity,
-  Dimensions,
-} from 'react-native';
-
-import arrowLeft from '../assets/icons/arrowLeft.svg';
-
-import * as Font from 'expo-font';
-
-export default function Memorywall({ navigation }) {
-  const currentYear = new Date().getFullYear();
-  const currentMonth = new Date().getMonth();
-
-  // Load the fonts
-  const loadFonts = async () => {
-    await Font.loadAsync({
-      moon: require('../assets/fonts/Moon.otf'),
-      manrope: require('../assets/fonts/Manrope.ttf'),
-      novatica: require('../assets/fonts/Novatica.ttf'),
-      novaticaBold: require('../assets/fonts/Novatica-Bold.ttf'),
-    });
-  };
-
-  useEffect(() => {
-    loadFonts();
-  }, []);
-
-  const [currentDate, setCurrentDate] = useState(new Date());
-  const [screenWidth, setScreenWidth] = useState(Dimensions.get('window').width);
-
-  useEffect(() => {
-    const updateScreenWidth = () => {
-      setScreenWidth(Dimensions.get('window').width);
-    };
-
-    Dimensions.addEventListener('change', updateScreenWidth);
-
-    return () => {
-      Dimensions.removeEventListener('change', updateScreenWidth);
-    };
-  }, []);
-
-  const getPreviousMonth = () => {
-    const previousMonth = new Date(
-      currentDate.getFullYear(),
-      currentDate.getMonth() - 1,
-      1
-    );
-    setCurrentDate(previousMonth);
-  };
-
-  const getDaysInMonth = (year, month) => {
-    const daysInMonth = new Date(year, month + 1, 0).getDate();
-    const daysArray = [];
-
-    for (let day = 1; day <= daysInMonth; day++) {
-      daysArray.push(new Date(year, month, day));
-    }
-
-    return daysArray;
-  };
-
-  const renderCalendarGrid = (month, today) => {
-    const year = month.getFullYear();
-    const monthIndex = month.getMonth();
-    const daysArray = getDaysInMonth(year, monthIndex);
-
-    const renderDaysOfWeek = () => {
-      const daysOfWeek = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
-      return (
-        <View style={styles.weekContainer}>
-          {daysOfWeek.map((day) => (
-            <Text key={day} style={styles.dayOfWeek}>
-              {day}
-            </Text>
-          ))}
-        </View>
-      );
-    };
-
-    const renderCalendarDays = () => {
-      const firstDayOfMonth = new Date(year, monthIndex, 1);
-      const firstDayOfWeek = firstDayOfMonth.getDay(); // 0 is Sunday, 1 is Monday, etc.
-      const emptyCells = Array(firstDayOfWeek).fill(null);
-
-      const calendarDays = emptyCells.concat(
-        daysArray,
-        Array(42 - daysArray.length - emptyCells.length).fill(null)
-      );
-
-      const rows = [];
-      const totalCells = calendarDays.length;
-      let rowCells = [];
-
-      calendarDays.forEach((day, index) => {
-        rowCells.push(day);
-
-        if ((index + 1) % 7 === 0 || index === totalCells - 1) {
-          rows.push(rowCells);
-          rowCells = [];
-        }
-      });
-
-      const dateContainerWidth = screenWidth / 7;
-
-      return (
-        <View style={styles.calendarGrid}>
-          {rows.map((row, rowIndex) => (
-            <View key={rowIndex} style={styles.weekContainer}>
-              {row.map((cell, cellIndex) => {
-                const isPastDate = cell && cell < today;
-                const backgroundColor = isPastDate ? '#FFF' : '#F2F2F2';
-
-                return (
-                  <View
-                    key={cellIndex}
-                    style={[
-                      styles.dateContainer,
-                      { width: dateContainerWidth, backgroundColor },
-                    ]}
-                  >
-                    {cell && (
-                      <ImageBackground
-                        source={require('../assets/grouppicture.jpg')}
-                        style={styles.dateImage}
-                      >
-                        <Text
-                          style={[
-                            styles.day,
-                            isPastDate && styles.currentDay,
-                          ]}
-                        >
-                          {cell && cell.getDate()}
-                        </Text>
-                      </ImageBackground>
-                    )}
-                  </View>
-                );
-              })}
-            </View>
-          ))}
-        </View>
-      );
-    };
-
-    return (
-      <View>
-        {renderDaysOfWeek()}
-        {renderCalendarDays()}
-      </View>
-    );
-  };
-
-  const monthsToShow = [
-    currentDate,
-    new Date(currentYear, currentMonth - 1, 1),
-    new Date(currentYear, currentMonth - 2, 1),
-  ];
-
-  return (
-    <View style={styles.container}>
-      <View style={styles.headerContainer}>
-      <TouchableOpacity onPress={() => navigation.goBack()} style={styles.arrowContainer}>
-        <Image source={arrowLeft} style={{ width: 8, height: 15 }} />
-      </TouchableOpacity>
-      <Text style={styles.heading}>Memorywall</Text>
-      </View>
-      <ScrollView>
-        {monthsToShow.map((month, index) => (
-          <View key={index} style={styles.calendarContainer}>
-            <Text style={styles.month}>
-              {month.toLocaleString('en-US', {
-                month: 'long',
-                year: 'numeric',
-              })}
-            </Text>
-            {renderCalendarGrid(month, new Date())}
-          </View>
-        ))}
-      </ScrollView>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#160635',
-  },
-  headerContainer: {
-    backgroundColor: '#160635',
-    height: 120,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  heading: {
-    color: '#fff',
-    fontSize: 20,
-    textAlign: 'center',
-    position: 'absolute',
-    top: 0,
-    width: '100%',
-    paddingTop: 50,
-    fontFamily: 'novaticaBold',
-  },
-  arrowContainer: {
-    position: 'absolute',
-    top: 20,
-    left: 15,
-    padding: 30,
-  },
-  calendarContainer: {
-    paddingHorizontal: 20,
-    paddingVertical: 20,
-    backgroundColor: "#fff",
-    borderTopLeftRadius: 25,
-    borderTopRightRadius: 25,
-  },
-  month: {
-    fontFamily: 'moon',
-    fontSize: 20,
-    marginBottom: 20,
-    textAlign: 'left',
-    color: '#160635',
-    fontWeight:'bold',
-  },
-  weekContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: 10,
-  },
-  dayOfWeek: {
-    fontFamily: 'manrope',
-    fontSize: 18,
-    color: '#160635',
-  },
-  calendarGrid: {
-    flexDirection: 'column',
-  },
-  dateContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    aspectRatio: 1,
-    marginBottom: 10,
-  },
-  dateImage: {
-    flex: 1,
-    resizeMode: 'contain',
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '40px',
-    borderRadius: '15px'
-  },
-  day: {
-    fontFamily: 'manrope',
-    fontSize: 20,
-    color: '#FFF',
-  },
-  currentDay: {
-    backgroundColor: 'blue',
-    backgroundImage: 'none'
-  },
-});
-*/
-
 import React, { useState, useEffect } from "react";
 import {
   View,
@@ -399,12 +126,10 @@ export default function Memorywall({ navigation }) {
                       styles.dateContainer, {backgroundColor, borderRadius}
                     ]}
                   >
-                    {cell && (
-                      <View style={styles.dateImageContainer}>
-                        {isPastDate && (
+                    {cell && isPastDate && (
                       <ImageBackground
                         source={require('../assets/grouppicture.jpg')}
-                        style={[styles.dateImage, {borderRadius:15},
+                        style={[styles.dateImage, {overflow: 'hidden', borderRadius},
                       ]}
                       >
                         <Text
@@ -416,11 +141,9 @@ export default function Memorywall({ navigation }) {
                           {cell.getDate()}
                         </Text>
                       </ImageBackground>
-                        )}
-                      </View>
                     )}
 
-                  {!isPastDate && (
+                  {cell && !isPastDate && (
                     <View style={styles.dateContent}>
                       <Text style={[styles.day, styles.futureDay]}>
                         {cell.getDate()}
@@ -517,9 +240,6 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     paddingVertical: 20,
     backgroundColor: "#fff",
-  },
-  dateImageContainer: {
-    borderRadius: 15
   },
   month: {
     fontFamily: 'moon',
