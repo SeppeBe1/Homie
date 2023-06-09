3f46da39a29c54d7f7272dd2c86801be2c0002ae
Jade Apers <jadeapers@telenet.be>
2023-06-09 15:51:42 +0200

    trying

--- homie/TabNavigator.js
+++ homie/TabNavigator.js
@@ -1,63 +1,98 @@
-import { View, Text, Image } from 'react-native'
-import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { View, Text, Image } from "react-native";
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import { HomescreenStack, BehomiescreenStack, CostsplitterscreenStack, CalendarScreenStack, LoginscreenStack } from "./StackNavigator";
+import {
+  HomescreenStack,
+  BehomiescreenStack,
+  CostsplitterscreenStack,
+  CalendarScreenStack,
+  LoginscreenStack,
+} from "./StackNavigator";
 
-const Homename = 'Home';
-const Costsplittername = 'Costsplitter';
-const Behomiename = 'Behomie';
-const Calendarname = 'Calendar';
-
+const Homename = "Home";
+const Costsplittername = "Costsplitter";
+const Behomiename = "Behomie";
+const Calendarname = "Calendar";
 
 const Tab = createBottomTabNavigator();
 
 // independent={true}
 
- export function TabNavigator() {
+export function TabNavigator() {
   return (
-        <Tab.Navigator screenOptions={{headerShown: false, showLabel: false}}>
-
-          <Tab.Screen name={Homename} component={HomescreenStack}
-            options={{
-              tabBarIcon: ({ focused, color, size }) => (
-                <Image
-                source={ focused ? require('./assets/icons/homeFocused.png') : require('./assets/icons/home.png')} 
-                style={{ width: size, height: size }}
-                resizeMode="contain"
-              />
-              )}}/>
-
-          <Tab.Screen name={Behomiename} component={BehomiescreenStack}
-          options={{
-            tabBarIcon: ({ focused, color, size }) => (
-              <Image
-              source={ focused ? require('./assets/icons/communityFocused.png') : require('./assets/icons/community.png')} 
+    <Tab.Navigator screenOptions={{ headerShown: false, showLabel: false }}>
+      <Tab.Screen
+        name={Homename}
+        component={HomescreenStack}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Image
+              source={
+                focused
+                  ? require("./assets/icons/homeFocused.png")
+                  : require("./assets/icons/home.png")
+              }
               style={{ width: size, height: size }}
               resizeMode="contain"
             />
-            )}}/>
+          ),
+        }}
+      />
 
-          <Tab.Screen name={Costsplittername} component={CostsplitterscreenStack}
-          options={{
-            tabBarIcon: ({ focused, color, size }) => (
-              <Image
-              source={ focused ? require('./assets/icons/costsplitFocused.png') : require('./assets/icons/costsplit.png')} 
+      <Tab.Screen
+        name={Behomiename}
+        component={BehomiescreenStack}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Image
+              source={
+                focused
+                  ? require("./assets/icons/communityFocused.png")
+                  : require("./assets/icons/community.png")
+              }
               style={{ width: size, height: size }}
               resizeMode="contain"
             />
-            )}}/>
+          ),
+        }}
+      />
 
-          <Tab.Screen name={Calendarname} component={CalendarScreenStack}
-          options={{
-            tabBarIcon: ({ focused, color, size }) => (
-              <Image
-              source={ focused ? require('./assets/icons/calendarFocused.png') : require('./assets/icons/calendar.png')} 
+      <Tab.Screen
+        name={Costsplittername}
+        component={CostsplitterscreenStack}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Image
+              source={
+                focused
+                  ? require("./assets/icons/costsplitFocused.png")
+                  : require("./assets/icons/costsplit.png")
+              }
+              style={{ width: size, height: size }}
+              resizeMode="contain"
+            />
+          ),
+        }}
+      />
+
+      <Tab.Screen
+        name={Calendarname}
+        component={CalendarScreenStack}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Image
+              source={
+                focused
+                  ? require("./assets/icons/calendarFocused.png")
+                  : require("./assets/icons/calendar.png")
+              }
               style={{ width: 35, height: 35 }}
               resizeMode="contain"
             />
-            )}}/>
-
-        </Tab.Navigator>
-  )
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
 }
--- homie/screens/login/Login.js
+++ homie/screens/login/Login.js
@@ -1,217 +1,224 @@
-import { StyleSheet, Text, View, TouchableOpacity, Image, TextInput } from 'react-native'
-import React, { useState }  from 'react'
-import  navigation  from '@react-navigation/native';
-import logo from "../../assets/logo.png";
-import logoHomie from "../../assets/logoHomie.svg";
+// import {
+//   StyleSheet,
+//   Text,
+//   View,
+//   TouchableOpacity,
+//   Image,
+//   TextInput,
+// } from "react-native";
+// import React, { useState } from "react";
+// import navigation from "@react-navigation/native";
+// import logo from "../../assets/logo.png";
+// import logoHomie from "../../assets/logoHomie.svg";
 
-import MoonFont from "../../assets/fonts/Moon.otf";
-import Novatica from "../../assets/fonts/Novatica-Bold.woff";
-import AsyncStorage from '@react-native-async-storage/async-storage';
+// import MoonFont from "../../assets/fonts/Moon.otf";
+// import Novatica from "../../assets/fonts/Novatica-Bold.woff";
+// import AsyncStorage from "@react-native-async-storage/async-storage";
 
+// export default function Login({ navigation }) {
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [color, setColor] = useState("white");
+//   const [validation, setValidation] = useState(false);
 
-export default function Login({ navigation }) {
+//   const Homename = "TabNavigator";
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [color, setColor] = useState('white');
-    const [validation, setValidation] = useState(false);
+//   const handleLogin = () => {
+//     // Perform login logic here, e.g., API calls, validation, etc.
+//     fetch("http://localhost:3000/api/v1/users/login", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({
+//         email: email,
+//         password: password,
+//       }),
+//     })
+//       .then((response) => response.json())
+//       .then((data) => {
+//         // Process the response data
+//         console.log(data.status);
 
-    const Homename = 'TabNavigator';
+//         if (data.status == "failed") {
+//           const newColor = color === "white" ? "#FF7A7A" : "white";
+//           setColor("#FF7A7A");
+//           setEmail("");
+//           setPassword("");
+//           setValidation(true);
+//         } else if (data.status == "succes") {
+//           if (!data.data.houseId) {
+//             let token = data.data.token;
+//             console.log(token);
+//             AsyncStorage.setItem("token", token);
+//             AsyncStorage.setItem("userId", data.data.userId);
+//             console.log(data.data.userId);
+//             navigation.navigate("HouseChoice");
+//           } else {
+//             let token = data.data.token;
+//             console.log(token);
+//             AsyncStorage.setItem("token", token);
+//             AsyncStorage.setItem("userId", data.data.userId);
+//             AsyncStorage.setItem("houseId", data.data.houseId);
+//             console.log(data.data.userId);
+//             navigation.navigate("TabNavigator");
+//           }
+//         }
+//         // Perform any necessary actions after successful login
+//       })
+//       .catch((error) => {
+//         // Handle any errors
+//         console.error(error);
+//       });
 
-    const handleLogin = () => {
-      // Perform login logic here, e.g., API calls, validation, etc.
-      fetch('http://localhost:3000/api/v1/users/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                email: email,
-                password: password,
-            }),
-            })
-            .then(response => response.json())
-            .then(data => {
-                // Process the response data
-                console.log(data.status);
+//     console.log("Email:", email);
+//     console.log("Password:", password);
+//   };
 
-                if(data.status == "failed"){
-                    const newColor = color === 'white' ? '#FF7A7A' : 'white';
-                    setColor('#FF7A7A');
-                    setEmail('');
-                    setPassword('');
-                    setValidation(true);
+//   return (
+//     <View style={styles.container}>
+//       <View style={styles.header}>
+//         <Image style={styles.logo} source={logoHomie} />
+//       </View>
 
-                } else if(data.status == "succes"){
-                  if(!data.data.houseId){
-                    let token = data.data.token
-                    console.log(token);
-                    AsyncStorage.setItem('token', token);
-                    AsyncStorage.setItem('userId', data.data.userId);
-                    console.log(data.data.userId);
-                    navigation.navigate("HouseChoice");
-                  } else{
-                    let token = data.data.token
-                    console.log(token);
-                    AsyncStorage.setItem('token', token);
-                    AsyncStorage.setItem('userId',  data.data.userId);
-                    AsyncStorage.setItem('houseId',  data.data.houseId);
-                    console.log(data.data.userId);
-                    navigation.navigate("TabNavigator");
-                  }
-                }
-                // Perform any necessary actions after successful login
-            })
-            .catch(error => {
-                // Handle any errors
-                console.error(error);
-            });
+//       <View style={styles.body}>
+//         <Text style={styles.h2}>Login to your account</Text>
 
-      console.log('Email:', email);
-      console.log('Password:', password);
-    };
+//         {validation && (
+//           <Text style={styles.validation}>Email or password incorrect</Text>
+//         )}
 
-  return (
-    <View style={styles.container}>
+//         <TextInput
+//           style={[styles.input, { borderColor: color }]}
+//           placeholder="Email"
+//           onChangeText={(text) => setEmail(text)}
+//           value={email}
+//         />
 
-        <View style={styles.header}>
-            <Image style={styles.logo}source={logoHomie}/>
-        </View>
+//         <TextInput
+//           style={[styles.input, { borderColor: color }]}
+//           placeholder="Password"
+//           secureTextEntry
+//           onChangeText={(text) => setPassword(text)}
+//           value={password}
+//         />
 
-        <View style={styles.body}>
-            <Text style={styles.h2}>Login to your account</Text>
+//         <Text style={styles.forgotPw} onPress={() => console.log("hallo")}>
+//           Forget password
+//         </Text>
 
-            {validation && (
-                <Text style={styles.validation}>Email or password incorrect</Text>
-             )}
+//         <TouchableOpacity style={styles.login} onPress={handleLogin}>
+//           <Text style={styles.buttonText}>Sign in</Text>
+//         </TouchableOpacity>
 
+//         <Text style={styles.noAcc}>
+//           Don't have an account yet?{" "}
+//           <Text
+//             style={styles.noAccCPress}
+//             onPress={() => navigation.navigate("SignUp")}
+//           >
+//             Sign up
+//           </Text>
+//         </Text>
+//       </View>
+//     </View>
+//   );
+// }
 
-            <TextInput
-            style={[styles.input, { borderColor: color }]}
-            placeholder="Email"
-            onChangeText={text => setEmail(text)}
-            value={email}
-            />
+// const styles = StyleSheet.create({
+//   container: {
+//     position: "relative",
+//     height: "100%",
+//     width: "100vw",
+//     backgroundColor: "#F9F9F9",
+//   },
 
-            <TextInput
-            style={[styles.input, { borderColor: color }]}
-            placeholder="Password"
-            secureTextEntry
-            onChangeText={text => setPassword(text)}
-            value={password}
-            />
+//   header: {
+//     backgroundColor: "#160635",
+//     height: 215,
+//     width: "100%",
+//   },
 
-            <Text style={styles.forgotPw} onPress={() => console.log("hallo")}>Forget password</Text>
+//   logo: {
+//     width: 280,
+//     height: "40%",
+//     marginTop: 50,
+//     marginLeft: "auto",
+//     marginRight: "auto",
+//   },
 
-            <TouchableOpacity style={styles.login} onPress={handleLogin}>
-            <Text style={styles.buttonText}>Sign in</Text>
-            </TouchableOpacity>
+//   body: {
+//     marginLeft: "auto",
+//     marginRight: "auto",
+//     width: 400,
+//   },
 
-            <Text style={styles.noAcc} >Don't have an account yet? <Text style={styles.noAccCPress}  onPress={() =>navigation.navigate("SignUp")}>Sign up</Text></Text>
-        </View>
-    </View>
-  )
-}
+//   h2: {
+//     textAlign: "center",
+//     fontSize: 25,
+//     fontFamily: MoonFont,
+//     marginTop: 60,
+//     marginBottom: 40,
+//   },
 
-const styles = StyleSheet.create({
+//   validation: {
+//     color: "#FF7A7A",
+//     fontSize: 16,
+//     paddingBottom: 10,
+//     marginTop: "-25px",
+//     textAlign: "center",
+//   },
 
-    container: {
-        position: "relative",
-        height: "100%",
-        width: "100vw",
-        backgroundColor:"#F9F9F9",
-      },
+//   forgotPw: {
+//     color: "#D9B2EE",
+//     marginTop: 10,
+//   },
 
-    header: {
-        backgroundColor: "#160635",
-        height: 215,
-        width: "100%",
-      },
+//   input: {
+//     fontSize: 16,
+//     fontFamily: Novatica,
+//     placeholderTextColor: "#A5A5A5",
+//     backgroundColor: "white",
 
-    logo: {
-        width: 280,
-        height: "40%",
-        marginTop: 50,
-        marginLeft: 'auto',
-        marginRight: 'auto',
-      },
+//     marginBottom: 17,
+//     padding: 17,
 
-    body: {
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        width: 400,
-      },
+//     borderWidth: 1,
+//     borderColor: "white",
+//     borderRadius: 10,
 
-      h2: {
-        textAlign: 'center',
-        fontSize: 25,
-        fontFamily: MoonFont,
-        marginTop: 60,
-        marginBottom: 40,
-      },
+//     width: "300",
+//   },
 
-      validation:{
-        color: "#FF7A7A",
-        fontSize: 16,
-        paddingBottom: 10,
-        marginTop: "-25px", 
-        textAlign: "center",
-      },
+//   login: {
+//     textAlign: "center",
+//     backgroundColor: "#B900F4",
+//     borderRadius: 100,
+//     width: 200,
 
-      forgotPw:{
-        color: "#D9B2EE",
-        marginTop: 10,
+//     paddingTop: 16,
+//     paddingRight: 66,
+//     paddingBottom: 16,
+//     paddingLeft: 66,
 
-      },
+//     marginTop: 46,
+//     marginLeft: "auto",
+//     marginRight: "auto",
+//   },
 
-      input:{
-        fontSize: 16,
-        fontFamily: Novatica,
-        placeholderTextColor:"#A5A5A5",
-        backgroundColor:"white",
+//   buttonText: {
+//     fontSize: 20,
+//     color: "white",
+//     fontFamily: MoonFont,
+//   },
 
-        marginBottom: 17,
-        padding: 17 ,
+//   noAcc: {
+//     color: "black",
+//     marginTop: 150,
+//     textAlign: "center",
+//   },
 
-        borderWidth: 1,
-        borderColor: 'white',
-        borderRadius: 10,
-
-        width: '300',
-      },
-
-      login:{
-        textAlign:"center",
-        backgroundColor:"#B900F4",
-        borderRadius: 100,
-        width: 200,
-
-        paddingTop: 16 ,
-        paddingRight: 66 ,
-        paddingBottom: 16  ,
-        paddingLeft: 66 ,
-
-        marginTop: 46,
-        marginLeft: 'auto',
-        marginRight: 'auto',
-      },
-
-      buttonText:{
-        fontSize: 20,
-        color: "white",
-        fontFamily: MoonFont,
-      },
-
-      noAcc:{
-        color:'black',
-        marginTop:150,
-        textAlign: "center",
-      },
-      
-      noAccCPress:{
-        color:'#D9B2EE',
-
-      },
-
-    
-})
+//   noAccCPress: {
+//     color: "#D9B2EE",
+//   },
+// });
